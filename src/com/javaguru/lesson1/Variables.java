bced766cc69eb5a07622e59669d583672951cb94
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2018-07-07 19:01:59 +0300

    Variables part1

---
+++ src/com/javaguru/lesson1/Variables.java
@@ -0,0 +1,9 @@
+package com.javaguru.lesson1;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        int integerNumber = 15;
+        System.out.println(integerNumber);
+    }
+}
