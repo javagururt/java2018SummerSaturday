2ce54204dc90827ed045f6b4dac6684b44bbdc41
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2018-07-14 19:58:40 +0300

    human demo

---
+++ src/com/javaguru/lesson2/Human.java
@@ -0,0 +1,23 @@
+package com.javaguru.lesson2;
+
+class Human {
+
+    private String name;
+    private int age;
+
+    public Human() {
+    }
+
+    public Human(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
---
+++ src/com/javaguru/lesson2/HumanDemo.java
@@ -0,0 +1,10 @@
+package com.javaguru.lesson2;
+
+class HumanDemo {
+
+    public static void main(String[] args) {
+        Human human = new Human("Vasya", 20);
+        System.out.println(human.getAge());
+        System.out.println(human.getName());
+    }
+}
