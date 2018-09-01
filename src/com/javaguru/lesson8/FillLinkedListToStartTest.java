d48fe85d7eaf5e900a148e949acefe19fd924606
Ruslans Tufetulovs <ruslans.tufetulovs@gmail.com>
2018-09-01 19:45:11 +0300

    fill linked list to start

---
+++ src/com/javaguru/lesson8/FillLinkedListToStartTest.java
@@ -0,0 +1,21 @@
+package com.javaguru.lesson8;
+
+import java.util.LinkedList;
+
+class FillLinkedListToStartTest {
+
+    public static void main(String[] args) {
+        LinkedList<Integer> integers = new LinkedList<>();
+        long start = System.currentTimeMillis();
+        fillArrayList(integers);
+        long finish = System.currentTimeMillis();
+        long result = finish - start;
+        System.out.println("Result: " + result + " ms");
+    }
+
+    public static void fillArrayList(LinkedList<Integer> integers) {
+        for (int i = 0; i < 10_000_000; i++) {
+            integers.add(0,i);
+        }
+    }
+}
